f9461fb3811443b6b5a4a452703c165ba7b45945
Rob Maes <remilmaes@gmail.com>
2021-05-01 13:57:44 -0500

    initial commit

---
+++ svelte.config.cjs
@@ -0,0 +1,21 @@
+const preprocess = require('svelte-preprocess');
+const adapter = require('@sveltejs/adapter-static');
+
+/** @type {import('@sveltejs/kit').Config} */
+module.exports = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
+
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: {
+			adapt: adapter
+		},
+		target: '#svelte'
+	}
+};
